4a68ab565d695f834f540ddbbe012b44d344dc55
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-04-10 10:23:50 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
  * main -Entry point
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float:%zu byte(s)\n", sizeof(float));
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+	printf("%d is positive\n", n);
+else if (n < 0)
+	printf("%d is negative\n", n);
+else
+	printf("%d is zero\n", n);
 
 	return (0);
 }
