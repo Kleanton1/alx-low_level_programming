67d88ed60d602544fcd5c07975a7f5df62299f90
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-04-12 15:26:35 -0700

    Nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_X10(void);
@@ -5,8 +8,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int, int );
 void print_to_98(int n);
+void print_times_table(int n);
+#endif /* _MAIN_M_ */
