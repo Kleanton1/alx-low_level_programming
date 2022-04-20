bdbb3eaa39ec7b7f365420d24bca2b4b4fa08e13
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-04-20 13:28:06 -0700

    Don't swap horses

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - swap the values of integers a and b 
+ * swap_int - swap the values of integers a and b
  * @a:A pointer to an int that will be updated
  * @b:B pointer to an int that will be updated
  * Return : void that means its correct
@@ -10,7 +10,7 @@
 void swap_int(int *a, int *b)
 {
 	int c;
-	
+
 	c = *a;
 	*a = *b;
 	*b = c;
