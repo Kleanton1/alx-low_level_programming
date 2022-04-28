a99164f1e6992949d51f020d865aecdda5ceb10e
kleanton1 <ifeanyiemmanuel271@gmail.com>
2022-04-28 16:19:25 -0700

    5sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int sqrt_check(int g, int c)
 {
 		if (g * g == c)
 			return (g);
-	if (g * g > c)
+		if (g * g > c)
 			return (-1);
 	return (sqrt_check(g + 1, c));
 }
@@ -25,6 +25,6 @@ int sqrt_check(int g, int c)
 int _sqrt_recursion(int n)
 {
 		if (n == 0)
-					return (0);
-			return (sqrt_check(1, n));
+			return (0);
+		return (sqrt_check(1, n));
 }
